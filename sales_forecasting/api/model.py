63875a08b0ffd2d6de54ba8db35333c1666b0346
Arslan Gurbanov <72892742+Gurbanov-Arslan@users.noreply.github.com>
2024-05-27 19:16:53 +0300

    Add files via upload

--- sales_forecasting/api/model.py
+++ sales_forecasting/api/model.py
@@ -1,1 +1,30 @@
+from pydantic import BaseModel, Field, validator
+from datetime import date
+from typing import Optional
+
+
+class RequestSchema(BaseModel):
+    sales_date: date = Field(alias="Date")
+    store: int = Field(alias="Store")
+    sales: int = Field(alias="Sales")
+    customers: Optional[int] = Field(alias="Customers")
+    open_flag: int = Field(alias="Open")
+    promo: int = Field(alias="Promo")
+    day_of_week: int = Field(alias="DayOfWeek")
+    state_holiday: Optional[str] = Field(alias="StateHoliday")
+    school_holiday: Optional[int] = Field(alias="SchoolHoliday")
+
+
+    @validator('store')
+    def check_negative_store_values(cls, v):
+        if v<0:
+            raise ValueError('Store column contains negative values')
+        return v
+    
+    @validator('sales')
+    def check_negative_sales_values(cls, v):
+        if v<0:
+            raise ValueError('Sales column contains negative values')
+        return v
+
 
